f7806a9fc90fc402a619a2de5c2b63af80d4c8f0
Carl Brithen <carl.brithen@idg.se>
2018-04-10 10:59:43 +0200

    php

---
+++ Test/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+    <form class="w_form" id="w_form" onSubmit="formConfirm()">
+        <h2>Intresseanmälan</h2>
+        <p>Lämna din mailadress så håller vi dig uppdaterad!</p>
+        <div class="w_inner">
+            <input type="email" name="email" id="wEmail" placeholder="din@epost.se*" required/>
+            <button>Skicka</button>
+        </div>
+        <div class="w_under">
+            <input type="checkbox" id="wTerms" required/>
+            <label for="wTerms">Jag accepterar IDG:s allmänna <a class="blue" href="#">villkor</a>*</label>
+        </div>
+    </form>
+    <script src="https://idg.webpower.eu/x/capture_form/webpower.js.php"></script>
+</body>
+</html>
