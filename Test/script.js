9646c67931cb9a19c2212de35a063d1b238e6d75
Carl Brithen <carl.brithen@idg.se>
2018-04-10 10:03:50 +0200

    Testsajt

---
+++ Test/script.js
@@ -0,0 +1,24 @@
+formConfirm = () => {
+	// prevent default action and bubbling
+	event.preventDefault();
+	event.stopPropagation();
+	 $.ajax({
+		 type: 'POST',
+		 url: location.href,
+		 success: function (data, responseText) {
+			 var overlay = document.createElement('div');
+			 overlay.className = "w_overlay";
+			 	var dialog = document.createElement('div');
+			 	dialog.className = "w_dialog";
+			 	var node = ("<h3>Tack!</h3>" +
+									 '<p>Mer information har skickats till din inkorg.</p>');
+			 	dialog.innerHTML = node;
+			 	overlay.appendChild(dialog);
+			 	document.getElementById('w_form').appendChild(overlay);
+		 },
+		 error: function (errorThrown) {
+				alert("Error");
+			 console.log(errorThrown);
+		 }
+	 });
+}
