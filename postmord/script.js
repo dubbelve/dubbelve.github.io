b46f91974620d8d9b6c2c9b4de2a4ad62215cef0
panagoracarl <carl.brithen@panagora.se>
2021-03-30 19:32:06 +0200

    postmord app

---
+++ postmord/script.js
@@ -0,0 +1,41 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		message: 'Ingen post idag!',
+		today: new Date()
+	},
+	created: function () {
+		const self = this;
+
+		self.isPost();
+	},
+	methods: {
+		isPost: function() {
+			let weeks = this.weeksBetween(new Date(2020, 02, 01), this.today);
+			let daynumber = this.today.getDay();
+
+			if (daynumber < 6) {
+				if (this.isEven(weeks)) {
+					if (!this.isEven(daynumber)) {
+						this.message = 'POST!'
+					}
+				} else {
+					if (this.isEven(daynumber)) {
+						this.message = 'POST!'
+					}
+				}
+			} else {
+				this.message = 'Det är helg...'
+			}
+		},
+		isEven: function(value) {
+			if (value%2 == 0)
+				return true;
+			else
+				return false;
+		},
+		weeksBetween: function(d1, d2) {
+			return Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
+		}
+	}
+})
